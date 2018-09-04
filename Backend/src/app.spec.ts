8446b319bd1410d8d19844f846ae3ae30758cb21
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-09-04 16:23:39 +0200

    start setting up for tests

--- Backend/src/app.spec.ts
+++ Backend/src/app.spec.ts
@@ -1,11 +1,9 @@
 import 'jasmine';
-import app from 'app'
+import app from "./app";
+
 
 describe('App', () => {
   it('should work', () => {
-      expect(2).toEqual(2)
+      expect(app).toBeTruthy()
   });
-  it('should work', () => {
-    expect(1).toEqual(1)
-});
 });
