88bf7b7af5ee34dbc2d1eb942780e8f8415f148f
oliwiah <oliwia.hajszprych@gmail.com>
2018-09-06 17:30:56 +0200

    created parallax and typing animation

---
+++ Frontend/src/app/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import {
+    MatToolbarModule,
+} from '@angular/material';
+
+@NgModule({
+    imports: [],
+    exports: [
+        MatToolbarModule,
+    ]
+}) export class MaterialModule {}
