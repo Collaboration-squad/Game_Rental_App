9f7959b06375ff4853d49e29f45beb8cbf02fcfb
oliwiah <oliwia.hajszprych@gmail.com>
2018-09-07 13:23:53 +0200

    added a draft of datatable

--- Frontend/src/app/material.module.ts
+++ Frontend/src/app/material.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import {
     MatToolbarModule,
+    MatTableModule,
 } from '@angular/material';
 
 @NgModule({
     imports: [],
     exports: [
+        MatTableModule,
         MatToolbarModule,
     ]
 }) export class MaterialModule {}
