d0bcdf4b9c8a55e1d2cb98d75ee2350997fb528d
oliwiah <oliwia.hajszprych@gmail.com>
2018-09-10 15:57:36 +0200

    styled datatable

--- Frontend/src/app/material.module.ts
+++ Frontend/src/app/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {
     MatToolbarModule,
     MatTableModule,
+    MatButtonModule,
 } from '@angular/material';
 
 @NgModule({
     imports: [],
     exports: [
+        MatButtonModule,
         MatTableModule,
         MatToolbarModule,
     ]
