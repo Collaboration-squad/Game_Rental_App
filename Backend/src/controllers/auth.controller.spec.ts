ae54e9a30c3df0f80c3b21a7569d8f4a6ca3589c
krzysztofbogusz91 <krzysztofbogusz050591@gmail.com>
2018-09-21 07:26:43 +0200

    add auth controller tests

---
+++ Backend/src/controllers/auth.controller.spec.ts
@@ -0,0 +1,34 @@
+import "jasmine";
+import * as request from "supertest";
+import * as nodeMocks from "node-mocks-http";
+import * as passport from "passport";
+
+import app from "../app";
+import { controller } from "./auth.controller";
+import { mockUser } from "./../mocks/users.mock";
+import { Request, Response, NextFunction } from "express";
+
+describe("AuthController", () => {
+  let req: Request;
+  let res: Response;
+  const next: NextFunction = () => null;
+
+  beforeEach(() => {
+    req = nodeMocks.createRequest({
+      method: "POST",
+      url: "/login",
+      body: mockUser
+    });
+    res = nodeMocks.createResponse();
+  });
+
+  it("should be able to call controller", () => {
+    const passportSpy = spyOn(passport, "authenticate").and.returnValue(
+      () => null
+    );
+    controller.onUserLogin(req, res, next);
+    console.log(res);
+
+    expect(passportSpy).toHaveBeenCalled();
+  });
+});
